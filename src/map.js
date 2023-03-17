0b618a5b193d9b5344f901f0c67b4020501998d9
viglino <jean-marc.viglino@ign.fr>
2023-03-17 09:45:25 +0100

    [ADD] print dialog

--- src/map.js
+++ src/map.js
@@ -27,13 +27,13 @@ const map = new Map({
 const plink = new Permalink({ visible: false })
 map.addControl(plink)
 
-map.addLayer(new Geoportail({ layer: 'GEOGRAPHICALGRIDSYSTEMS.PLANIGNV2' }))
-
 // MVT Layer
-const layer = plink.getUrlParam('layer') || 'erp'
+const layer = plink.getUrlParam('layer') || 'cyanotype'
 const mvt = new MVT({
   url: './' + layer + '/' + layer + '.json'
 })
+
+map.addLayer(new Geoportail({ layer: 'GEOGRAPHICALGRIDSYSTEMS.PLANIGNV2', visible: /^erp/.test(layer) }))
 map.addLayer(mvt)
 
 map.addControl(new LayerSwitcher)
@@ -82,7 +82,35 @@ map.addInteraction(hover)
 
 // Print
 import PrintDialog from 'ol-ext/control/PrintDialog'
-map.addControl(new PrintDialog)
+import { jsPDF } from 'jspdf';
+import { saveAs } from 'file-saver';
+
+printControl = new PrintDialog;
+map.addControl(printControl)
+printControl.on(['print', 'error'], function(e) {
+  // Print success
+  if (e.image) {
+    if (e.pdf) {
+      // Export pdf using the print info
+      var pdf = new jsPDF({
+        orientation: e.print.orientation,
+        unit: e.print.unit,
+        format: e.print.size
+      });
+      pdf.addImage(e.image, 'JPEG', e.print.position[0], e.print.position[0], e.print.imageWidth, e.print.imageHeight);
+      pdf.save(e.print.legend ? 'legend.pdf' : 'map.pdf');
+    } else  {
+      // Save image as file
+      e.canvas.toBlob(function(blob) {
+        var name = (e.print.legend ? 'legend.' : 'map.')+e.imageType.replace('image/','');
+        saveAs(blob, name);
+      }, e.imageType, e.quality);
+    }
+  } else {
+    console.warn('No canvas to export');
+  }
+});
+
 
 // Search tools
 import SearchGeoportail from 'ol-ext/control/SearchGeoportail'
